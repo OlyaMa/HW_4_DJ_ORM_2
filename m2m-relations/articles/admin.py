68ca7a41b6d4f7852c1029d24079fa5735490dce
Olga Markus <markus.olya@yandex.ru>
2023-03-20 16:28:14 +0300

    Initial commit

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            if form.cleaned_data.get('is_main') is True:
+                counter += 1
+        if counter > 1:
+            raise ValidationError('Основным может быть только один раздел')
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+        if counter == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
